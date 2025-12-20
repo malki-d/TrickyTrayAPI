61e245afee0f8c6b7a5e31b3ee5d2346d55dcd51
malki-d <d32603260@gmail.com>
2025-12-20 22:36:43 +0200

    initial commit

---
+++ TrickyTrayAPI/Models/Donor.cs
@@ -0,0 +1,14 @@
+﻿namespace TrickyTrayAPI.Models
+{
+    public class Donor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+    }
+}
