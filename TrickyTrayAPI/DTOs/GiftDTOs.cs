f6aa9ac772a848ca842eb3a3ac18e266420e6c23
malki-d <d32603260@gmail.com>
2026-01-19 18:37:11 +0200

    ...

--- TrickyTrayAPI/DTOs/GiftDTOs.cs
+++ TrickyTrayAPI/DTOs/GiftDTOs.cs
@@ -6,6 +6,7 @@ namespace TrickyTrayAPI.DTOs
 {
     public class GetGiftDTO
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string ImgUrl { get; set; }
